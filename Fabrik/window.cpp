4f7f0d61e8ca049e9af4db4e33e16d5916f043af
Franhisco <frjune10@gmail.com>
2021-10-23 18:23:44 +0200

    Se anade los ficheros iniciales. Todavia no compilan

---
+++ Fabrik/window.cpp
@@ -0,0 +1,50 @@
+#include "glwidget.h"
+#include "window.h"
+#include "mainwindow.h"
+#include <QSlider>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QKeyEvent>
+#include <QPushButton>
+#include <QDesktopWidget>
+#include <QApplication>
+#include <QMessageBox>
+
+Window::Window(MainWindow *mw)
+    : mainWindow(mw)
+{
+    glWidget = new GLWidget;
+
+    xSlider = createSlider();
+    ySlider = createSlider();
+    zSlider = createSlider();
+
+    connect(xSlider, &QSlider::valueChanged, glWidget, &GLWidget::setXRotation);
+    connect(glWidget, &GLWidget::xRotationChanged, xSlider, &QSlider::setValue);
+    connect(ySlider, &QSlider::valueChanged, glWidget, &GLWidget::setYRotation);
+    connect(glWidget, &GLWidget::yRotationChanged, ySlider, &QSlider::setValue);
+    connect(zSlider, &QSlider::valueChanged, glWidget, &GLWidget::setZRotation);
+    connect(glWidget, &GLWidget::zRotationChanged, zSlider, &QSlider::setValue);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QHBoxLayout *container = new QHBoxLayout;
+    container->addWidget(glWidget);
+    container->addWidget(xSlider);
+    container->addWidget(ySlider);
+    container->addWidget(zSlider);
+
+    QWidget *w = new QWidget;
+    w->setLayout(container);
+    mainLayout->addWidget(w);
+    dockBtn = new QPushButton(tr("Undock"), this);
+    connect(dockBtn, &QPushButton::clicked, this, &Window::dockUndock);
+    mainLayout->addWidget(dockBtn);
+
+    setLayout(mainLayout);
+
+    xSlider->setValue(15 * 16);
+    ySlider->setValue(345 * 16);
+    zSlider->setValue(0 * 16);
+
+    setWindowTitle(tr("Hello GL"));
+}
---
+++ Fabrik/window.h
@@ -0,0 +1,39 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QSlider;
+class QPushButton;
+QT_END_NAMESPACE
+
+class GLWidget;
+class MainWindow;
+
+class Window : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Window(MainWindow *mw);
+
+signals:
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+private slots:
+    void dockUndock();
+
+private:
+    QSlider *createSlider();
+
+    GLWidget *glWidget;
+    QSlider *xSlider;
+    QSlider *ySlider;
+    QSlider *zSlider;
+    QPushButton *dockBtn;
+    MainWindow *mainWindow;
+};
+
+#endif // WINDOW_H
